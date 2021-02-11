39b32b63885bca45b5227bb07f7df6e4cd95102c
Michał Śledź <sledzson08@gmail.com>
2021-02-11 19:30:25 +0100

    Implement very first version of client

--- c_src/ex_quic/client.h
+++ c_src/ex_quic/client.h
@@ -3,11 +3,35 @@
 #include <unifex/unifex.h>
 #include <pthread.h>
 
-typedef struct CNodeState CNodeState;
+#define EV_STANDALONE 1
+#define EV_API_STATIC 1
+#include "ev.c"
+#include "lsquic_utils.h"
 
-struct CNodeState {
+typedef struct State State;
+
+struct State {
     UnifexEnv *env;
+
+    // event loop
+    struct ev_loop *loop;
+    ev_io sock_watcher;
+    ev_timer conn_watcher;
     pthread_t event_loop_tid;
+
+    // lsquic
+    int sockfd;
+    struct sockaddr_storage local_sas;
+    struct sockaddr_in peer_addr;
+    lsquic_engine_t *engine;
+    lsquic_conn_t *conn;
+    lsquic_stream_t *stream;
+    struct lsquic_engine_settings engine_settings;
+    struct lsquic_engine_api engine_api;
+
+    // msg to send
+    char *buf;
+    int size;
 };
 
 #include "_generated/client.h"
--- c_src/ex_quic/client.spec.exs
+++ c_src/ex_quic/client.spec.exs
@@ -2,7 +2,10 @@ module ExQuic.Client
 
 interface CNode
 
-state_type "CNodeState"
+state_type "State"
 
-spec init(local_ip :: string, local_port :: int, remote_ip :: string, remote_port :: int) :: {:ok :: label, state}
+spec init(remote_ip :: string, remote_port :: int) :: {:ok :: label, state}
 
+spec send_payload(state, data :: payload) :: {:ok :: label, state}
+
+sends {:quic_payload :: label, payload :: payload}
